6bd99449eec09c7391bd3297a8ebdb4301fbde2d
Patrick Leite Oliveira <titik.leite22@gmail.com>
2023-10-03 07:38:26 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,25 @@
+public class D {
+    private int D1;
+    private float D2;
+    
+    public int getD1() {
+        return D1;
+    }
+    public float getD2() {
+        return D2;
+    }
+    public void setD1(int d1) {
+        D1 = d1;
+    }
+    public void setD2(float d2) {
+        D2 = d2;
+    }
+    
+    public void MD1(){
+        System.out.println("metodo MD1");
+    }
+
+    public void MD2(){
+        System.out.println("metodo MD2");
+    }
+}
